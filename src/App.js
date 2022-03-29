b76f4a1f799c426a4451723f6bd7e74dccf51d64
Yahya EL Ganayni <76224648+yahyaelganyni1@users.noreply.github.com>
2022-03-29 09:10:00 +0200

    Create Header components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import React, { Component } from 'react'
+import Header from './components/Header';
 import './App.css';
 
-import React, { Component } from 'react'
-
 export default class App extends Component {
+
+
+
   render() {
     return (
-      <div className="App"> App</div>
+      <div className="App">
+        <Header />
+      </div>
     )
   }
 }
