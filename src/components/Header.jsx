8af4e0628526855f853bb2f1518f20d49488237b
Yahya EL Ganayni <76224648+yahyaelganyni1@users.noreply.github.com>
2022-03-29 11:46:25 +0200

    Add header components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,29 @@
 import React, { Component } from 'react';
-
+import logo from '../images/logo.svg';
+import dropdown from '../images/dropdown.svg';
+import cart from '../images/cart.svg';
+import './header.scss';
 export default class Header extends Component {
   render() {
-    return <div>Header</div>;
+    return (
+      <header className="header">
+        <nav>
+          <ul>
+            <li>women</li>
+            <li>men</li>
+            <li>kids</li>
+          </ul>
+          <img src={logo} alt="website logo" />
+          <ul>
+            <li>
+              $ <img src={dropdown} alt="dropdown logo" />
+            </li>
+            <li>
+              <img src={cart} alt="cart logo" />
+            </li>
+          </ul>
+        </nav>
+      </header>
+    );
   }
 }
